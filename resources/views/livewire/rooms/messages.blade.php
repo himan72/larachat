ceea6a98c8f1abf6daba6cf1b0bb6c73622bca92
Mohammed Hicham <mohammed@heber-net.net>
2020-08-09 19:55:00 +0100

    done

--- resources/views/livewire/rooms/messages.blade.php
+++ resources/views/livewire/rooms/messages.blade.php
@@ -1,20 +1,9 @@
 <div>
-    <ul class="chat-box chatContainerScroll">
-        <ul class="list-group">
-            @forelse($messages as $message)
-                <li class="chat-left">
-                    <div class="chat-avatar">
-                        <img src="https://www.bootdey.com/img/Content/avatar/avatar1.png" alt="Retail Admin">
-                        <div class="chat-name">{{$message->user->name}}</div>
-                    </div>
-                    <div class="chat-text">
-                        {{ $message->body }}?
-                    </div>
-                    <div class="chat-hour">{{ $message->created_at->diffForHumans() }} </div>
-                </li>
-                <li class="list-group-item">{{ $message->body }}</li>
-            @empty
-                <p>Nothing here yet</p>
-            @endforelse
-        </ul>
+    <ul class="list-group">
+        @forelse($messages as $message)
+            <li class="list-group-item">{{ $message->body }}</li>
+        @empty
+            <p>Nothing here yet</p>
+        @endforelse
+    </ul>
 </div>

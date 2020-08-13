670b17c3a630be9da400968893ae7da9572bbb0f
Mohammed Hicham <mohammed@heber-net.net>
2020-08-13 11:34:26 +0100

    livewire uesrs compont added

---
+++ resources/views/livewire/rooms/users.blade.php
@@ -0,0 +1,11 @@
+<ul class="users">
+    <li class="person">
+        <div class="user">
+            <img src="https://www.bootdey.com/img/Content/avatar/avatar3.png" alt="Retail Admin">
+
+        </div>
+        <p class="name-time">
+            <span class="name">User 1</span>
+        </p>
+    </li>
+</ul>
--- resources/views/rooms/show.blade.php
+++ resources/views/rooms/show.blade.php
@@ -5,7 +5,17 @@
             <h2 class="my-4">{{ $room->title }}</h2>
             <!-- Row start -->
             <div class="row gutters">
-                <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-3 col-3">
+                    <div class="users-container">
+                        <div class="chat-search-box">
+                            <div class="input-group">
+                                <h3>Liste des Utilisateurs</h3>
+                            </div>
+                        </div>
+                        <livewire:rooms.users />
+                    </div>
+                </div>
+                <div class="col-xl-8 col-lg-8 col-md-8 col-sm-12 col-12">
                     <div class="card m-0">
                         <!-- Row start -->
                         <div class="row no-gutters">
